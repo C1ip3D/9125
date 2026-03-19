78580f802e1ec7b8a9332a1f0a5516c3da3d64bd
Avaneesh Shetti <shettiavaneesh@gmail.com>
2026-03-18 21:59:10 -0700

    Auton Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,8 +15,11 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.units.Units;
+import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -48,6 +51,7 @@ public class Robot extends LoggedRobot {
     public TransportSubsystem transport;
 
     public Command autonomousCommand;
+    private SendableChooser<Command> autoChooser;
     public AutoAim autoaim;
     public ShooterAim shooterAim;
 
@@ -82,7 +86,7 @@ public class Robot extends LoggedRobot {
     }
 
     public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
+        return autoChooser.getSelectedCommand();
     }
 
     @Override
@@ -95,6 +99,9 @@ public class Robot extends LoggedRobot {
         shooterAim = new ShooterAim(drivebase, shooter);
 
         configureBindings();
+
+        autoChooser = AutoBuilder.buildAutoChooser();
+        SmartDashboard.putData("Auto Chooser", autoChooser);
 
         // AdvantageKit
         Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
@@ -179,7 +186,7 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void autonomousInit() {
-        autonomousCommand = Commands.print("we should probably make an auton");
+        autonomousCommand = getAutonomousCommand();
         if (autonomousCommand != null) {
             CommandScheduler.getInstance().schedule(autonomousCommand);
         }
